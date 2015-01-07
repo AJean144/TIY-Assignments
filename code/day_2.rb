0b21c798b66e40e1ab410e81d4db5c153997624d
Manatazach Lawya <manatazach144@manatazachs-mbp.attlocal.net>
2015-01-07 06:19:30 -0500

    change function and allow in to accept number names from one to eight

--- code/day_2.rb
+++ code/day_2.rb
@@ -1,82 +1,57 @@
 
-# my_nums = {
-# 	1 => "one",
-# 	2 => "two", 
-# 	3 => "three", 
-# 	4 => "four", 
-# 	5 => "five", 
-# 	6 => "six", 
-# 	7 => "seven", 
-# 	8 => "eight", 
-# 	9 => "nine", 
-# 	10 => "ten"
-# }
 
-def numConverter(number_name)
+# def numConverter(number_name)
 		
- one_to_twenty = ['one',
-				  'two', 
-				  'three',
-				  'four',
-				  'five',
-				  'six',
-				  'seven',
-				  'eight',
-				  'nine',
-				  'ten',
-				  'eleven',
-	      		  'twelve', 
-	      		  'thirteen', 
-	      		  'fourteen', 
-	      		  'fifteen', 
-	      		  'sixteen', 
-	      		  'seventeen', 
-	      		  'eighteen', 
-	      		  'nineteen',
-	      		  'twenty',
-				  ]
+# one_to_twenty = ['one','two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty']
 
-	 tens_hash = ['thirty',
-				  'fourty',
-				  'fifty',
-				  'sixty',
-				  'seventy',
-				  'eighty',
-				  'ninety'
-				  ]
+# tens = ['thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
-	if number_name == ones_hash["seven"]
-		return ones_hash[number_name]
+# add_one = number_name - 1
+
+# 	if one_to_twenty[number_name] == one_to_twenty[0]
+# 		puts 'one'
+# 	end
+# end
+
+# puts numConverter (1)
+
+# one_to_twenty = ['one','two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty']
+
+
+# puts one_to_twenty[0]
+
+
+
+# I have a hash of number names and their integer counterparts
+# I want the integer based on the number name.
+# number_hash[number_name]
+# if I find the name of the number in the hash, print the integer, if not, print "This number exceeds my limit... Opps"
+
+def num_converter(number_name)
+	number_hash = {'one' => 1, 
+				  'two' => 2, 
+				  'three' => 3,
+				  'four' => 4,
+				  'five' => 5,
+				  'six' => 6,
+				  'seven' => 7,
+				  'eight' => 8
+				}
+	# number_hash[number_name] || 0
+	if number_hash[number_name]
+		number_hash[number_name]
 	end
 
-	#if name_hash[first_name]
-		#name_hash[first_name]
-	#else
+	if number_name == 'three'
+		puts 3
+	elsif number_name == 'seven'
+		puts 7
+	else 
+		"This number exceeds my limit... Opps"
+	end
+end	
 
-	#if first-name == 'Spencer'
-		#'Hopkins'
-	#elsif first_name == 'Kelly'
-		#'Strickland'
-	#else 
-		#"I'm afraid I can't do that, Dave"
-	#end
-end
-# puts numConverter ("seven")
-
-
-# 	 ones_hash = {'one' => 1,
-# 				  'two' => 2, 
-# 				  'three' => 3,
-# 				  'four' => 4,
-# 				  'five' => 5,
-# 				  'six' => 6,
-# 				  'seven' => 7,
-# 				  'eight' => 8,
-# 				  'nine' => 9
-# 				  }
-
-# puts ones_hash["seven"]
-
-
-
-
+puts num_converter('seven')
+puts num_converter('two') 
+puts num_converter('five')
+puts num_converter('three')
