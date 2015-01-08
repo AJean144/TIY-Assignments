94be54ee7cc555b51b04bcf30a92b3426e4de03e
Manatazach Lawya <manatazach144@manatazachs-mbp.attlocal.net>
2015-01-08 07:33:12 -0500

    update to_fac method and call to_fac within itself

--- code/fac_method.rb
+++ code/fac_method.rb
@@ -1,33 +1,15 @@
 # Create a to_factorial method
 
-# class Fixnum
-
-# 	def to_fac
-# 		if self == 0
-# 		  self = 1
-# 		else
-# 		  self * (self - 1)
-# 		end
-# 	end
-# end
-
-# puts 4.to_fac
 
 class Fixnum
 
-	def fac_num
-		if self == 1
-			1
-		elsif self == 2
-			2 * 1
-		elsif self == 3
-			3 * 2 * 1
-		elsif self == 4
-			4 * 3 * 2 * 1
+	def to_fac
+		if self == 0
+			1 
 		else
-		    5 * 4 * 3 * 2 * 1 		
+			self * (self - 1).to_fac
 		end
 	end
 end
 
-puts 4.fac_num
+puts 12.to_fac #=> 479001600
