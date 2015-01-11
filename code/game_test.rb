8e6a77729dcdc9d07b3e20687e2a0367c99570b2
Manatazach Lawya <manatazach144@Manatazachs-MacBook-Pro.local>
2015-01-10 23:57:54 -0500

    add comments of every noun and verb I could think of in game

--- code/game_test.rb
+++ code/game_test.rb
@@ -5,6 +5,25 @@ require './game'
 
 class TestGame < MiniTest::Unit::TestCase
 
+#This will be a game of black jack. 
+
+#Deck
+  #The deck should have a 2,3,4,5,6,7,8,9,10 and then face cards like jack, queen, king and ace. 
+  #Each face card is the same as 10 except for the ace which is 11.
+  #The deck will have these values for times in the deck.
+
+#Dealer
+  #The dealer will be able to randomly give a card to the player and must deal 2 cards only at the start of game.
+  #When the player says hit, dealer must give one random card. Every card delt must remove a card from the deck.
+  #The dealer must stop if players draws to 17. 
+
+#Hand
+  #Hand must receive 2 cards at the begining of the game. 
+  #Must be able to hit
+  #If the hand gets to 17 or higher then cannot hit anymore
+  #If hand gets more than 21 then hand loses
+  #If hand has more than than player and player is over 17 then player wins
+
   def setup
     @game = Game.new
   end
